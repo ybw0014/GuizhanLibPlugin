5c21e8655ce15c4286f015c99878efb0a98a1d65
ybw0014 <i@guizhanss.com>
2023-06-10 04:39:49 -0400

    Language file fallback

--- src/main/java/net/guizhanss/guizhanlibplugin/setup/MinecraftLanguageSetup.java
+++ src/main/java/net/guizhanss/guizhanlibplugin/setup/MinecraftLanguageSetup.java
@@ -5,6 +5,7 @@ import lombok.experimental.UtilityClass;
 import net.guizhanss.guizhanlib.minecraft.LanguageHelper;
 import net.guizhanss.guizhanlibplugin.GuizhanLibPlugin;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.io.InputStream;
 import java.util.logging.Level;
@@ -24,15 +25,30 @@ public final class MinecraftLanguageSetup {
      */
     @ParametersAreNonnullByDefault
     public static void setup(final GuizhanLibPlugin plugin) {
-        final String minecraftVersion = "1." + PaperLib.getMinecraftVersion();
-        final String filename = "/minecraft-lang/" + minecraftVersion + "/zh_cn.json";
-        final InputStream stream = plugin.getClass().getResourceAsStream(filename);
-
-        if (stream != null) {
-            plugin.getLogger().log(Level.INFO, "Loading language file for Minecraft version " + minecraftVersion);
-            LanguageHelper.loadFromStream(stream);
-        } else {
-            throw new UnsupportedOperationException("The language file for Minecraft version " + minecraftVersion + " is missing!");
+        int mcVersion = PaperLib.getMinecraftVersion();
+        InputStream stream;
+        while (mcVersion >= 16) {
+            plugin.getLogger().log(Level.INFO, "Finding language file for Minecraft version 1." + mcVersion);
+            stream = getLanguageStream(plugin, "1." + mcVersion);
+            if (stream != null) {
+                plugin.getLogger().log(Level.INFO, "Loading language file for Minecraft version 1." + mcVersion);
+                LanguageHelper.loadFromStream(stream);
+                break;
+            } else {
+                plugin.getLogger().log(Level.INFO, "The language file for Minecraft version 1." + mcVersion + "is " +
+                    "missing, finding the language file for previous version");
+                mcVersion--;
+            }
         }
+
+        throw new UnsupportedOperationException("There is no available language file, you are using an unsupported " +
+            "Minecraft version!");
+    }
+
+    @Nullable
+    @ParametersAreNonnullByDefault
+    private static InputStream getLanguageStream(final GuizhanLibPlugin plugin, String minecraftVersion) {
+        final String filename = "/minecraft-lang/" + minecraftVersion + "/zh_cn.json";
+        return plugin.getClass().getResourceAsStream(filename);
     }
 }
